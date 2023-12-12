020a3c580860721a5d86e2015aab788e632b7eb2
mark8044 <87546778+mark8044@users.noreply.github.com>
2023-12-12 13:28:55 -0800

    Update appflowy_editor.dart

--- lib/appflowy_editor.dart
+++ lib/appflowy_editor.dart
@@ -1,27 +1,27 @@
-/// AppFlowyEditor library
+//// AppFlowyEditor library
 library appflowy_editor;
 
-// core part, including document, node, selection, etc.
+/// core part, including document, node, selection, etc.
 export 'src/core/core.dart';
 export 'src/editor/block_component/rich_text/appflowy_rich_text.dart';
 export 'src/editor/block_component/rich_text/appflowy_rich_text_keys.dart';
-// legacy
+/// legacy
 export 'src/editor/block_component/rich_text/default_selectable_mixin.dart';
-// editor part, including editor component, block component, etc.
+/// editor part, including editor component, block component, etc.
 export 'src/editor/editor.dart';
 export 'src/editor/find_replace_menu/find_and_replace.dart';
 export 'src/editor/l10n/appflowy_editor_l10n.dart';
 export 'src/editor/selection_menu/selection_menu.dart';
-// editor state
+/// editor state
 export 'src/editor_state.dart';
-// extension
+/// extension
 export 'src/extensions/extensions.dart';
 export 'src/infra/clipboard.dart';
 export 'src/infra/flowy_svg.dart';
 export 'src/infra/log.dart';
 export 'src/infra/mobile/mobile.dart';
 export 'src/l10n/l10n.dart';
-// plugins part, including decoder and encoder.
+/// plugins part, including decoder and encoder.
 export 'src/plugins/plugins.dart';
 export 'src/render/selection/selectable.dart';
 export 'src/render/toolbar/toolbar_item.dart';
